bc8edb951bf6b4da90c8ec72e725c2dfd90cec0a
Jonathan Arnett <jonarnett90@gmail.com>
2014-08-16 11:00:14 -0400

    Complete port to Foreman. Needs testing

--- lib/guard/foreman.rb
+++ lib/guard/foreman.rb
@@ -2,31 +2,17 @@ require 'guard'
 require 'guard/guard'
 
 module Guard
-  class Unicorn < Guard
-
-    # Sensible defaults for Rails projects
-    DEFAULT_PID_PATH    = File.join("tmp", "pids", "unicorn.pid")
-    DEFAULT_CONFIG_PATH = File.join("config", "unicorn.rb")
-    DEFAULT_PORT        = 3000
-    DEFAULT_ENVIRONMENT = "development"
+  class Foreman < Guard
 
     # Initialize a Guard.
     # @param [Array<Guard::Watcher>] watchers the Guard file watchers
     # @param [Hash] options the custom Guard options
     def initialize(watchers = [], options = {})
-      if watchers.empty?
-        watchers << Watcher.new( /^app\/(controllers|models|helpers)\/.+\.rb$/ )
-        watchers << Watcher.new( /^lib\/.+\.rb$/ )
-      end
-
-      @run_as_daemon  = options.fetch(:daemonize, false)
-      @enable_bundler = options.fetch(:bundler, true) 
-      @pid_file       = options.fetch(:pid_file, DEFAULT_PID_PATH)
-      @config_file    = options.fetch(:config_file, DEFAULT_CONFIG_PATH)
-      @preloading     = options.fetch(:preloading, false)
-      @port           = options.fetch(:port, DEFAULT_PORT)
-      @environment    = options.fetch(:environment, DEFAULT_ENVIRONMENT)
-      @socket         = options.fetch(:socket, nil)
+      @concurrency    = options[:concurrency]
+      @env            = options[:env]
+      @procfile       = options[:procfile]
+      @port           = options[:port]
+      @root           = options[:root]
 
       super
     end
@@ -34,66 +20,61 @@ module Guard
     # Call once when Guard starts. Please override initialize method to init stuff.
     # @raise [:task_has_failed] when start has failed
     def start
-      # Make sure unicorn is stopped
-      stop
+      # Stop if running
+      stop if pid
 
       cmd = []
-      cmd << "bundle exec" if @enable_bundler
-      cmd << "unicorn_rails"
-      cmd << "-c #{@config_file}"
-      cmd << "-p #{@port}" if @port
-      cmd << "-l #{@socket}" if @socket
-      cmd << "-E #{@environment}"
-      cmd << "-D" if @run_as_daemon 
+      cmd << "foreman start"
+      cmd << "-c #{@concurrency}" if @concurrency
+      cmd << "-e #{@env}"         if @env
+      cmd << "-f #{@procfile}"    if @profile
+      cmd << "-p #{@port}"        if @port
+      cmd << "-d #{@root}"        if @root
+
 
       @pid = ::Process.fork do
         system "#{cmd.join " "}"
       end
 
-      success "Unicorn started."
+     success "Foreman started."
     end
 
     # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
     # @raise [:task_has_failed] when stop has failed
     def stop
-      return unless pid
-
       begin
         ::Process.kill("QUIT", pid) if ::Process.getpgid(pid)
 
-        # Unicorn won't always shut down right away, so we're waiting for
+        # foreman won't always shut down right away, so we're waiting for
         # the getpgid method to raise an Errno::ESRCH that will tell us
         # the process is not longer active.
         sleep 1 while ::Process.getpgid(pid)
-        success "Unicorn stopped."
+        success "Foreman stopped."
       rescue Errno::ESRCH
         # Don't do anything, the process does not exist
       end
     end
 
     # Called when `reload|r|z + enter` is pressed.
-    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
+    # This method should be mainly used for "reload" (really!) actions like
+    # reloading passenger/spork/bundler/...
     # @raise [:task_has_failed] when reload has failed
     def reload
       # If the `preload_app` directive is false, then the workers will pick up
       # any code changes using a `HUP` signal, but if the application is
       # preloaded, then a `USR2 + QUIT` signal must be used.
-      #
-      # For now, let's rely on the fact that the user does know how to write
-      # a good unicorn configuration and he will have a block of code that
-      # will properly handle `USR2` signals.
       if @preloading
         oldpid = pid
-        UI.debug "Sending USR2 to unicorn with pid #{oldpid}"
+        UI.debug "Sending USR2 to Foreman with pid #{oldpid}"
         ::Process.kill 'USR2', oldpid
-        UI.debug "Sending QUIT to unicorn with pid #{oldpid}"
+        UI.debug "Sending QUIT to Foreman with pid #{oldpid}"
         ::Process.kill 'QUIT', oldpid
       else
         ::Process.kill 'HUP', pid
       end
 
-      UI.info "Done reloading unicorn."
-      success "Unicorn reloaded"
+      UI.info "Done reloading Foreman."
+      success "Foreman reloaded"
     end
 
     # Called when just `enter` is pressed
@@ -114,7 +95,7 @@ module Guard
       # Favor the pid in the pidfile, since some processes
       # might daemonize properly and fork twice.
       if File.exists?(@pid_file)
-        @pid = File.open(@pid_file) { |f| f.gets.to_i } 
+        @pid = File.open(@pid_file) { |f| f.gets.to_i }
       end
 
       @pid
